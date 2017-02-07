aa4e9216a386eee09377c3ffba6549268ea6e129
Ologliniel <delphine.bourdelle@hotmail.fr>
2017-02-07 14:40:35 +0100

    verif compil

--- src/core/entities/User.java
+++ src/core/entities/User.java
@@ -1,58 +1,55 @@
-Public Class User
+import java.util.ArrayList;
+
+public class User
 {
-	String nom;
-	String prenom;
-	String login;
-	String password;
+	private String nom;
+	private String prenom;
+	private String login;
+	private String password;
 
 	public User(){
 		ArrayList<Droits> listeDroits = new ArrayList<Droits>();
 	}
 
+	public User(String nom, String prenom, String login, String password) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.login = login;
+		this.password = password;
+	}
 
-Public User() {
-
-}
-
-Public User(String nom, String prenom, String login, String password) {
-	this.nom = nom;
-	this.prenom = prenom;
-	this.login = login;
-	this.password = password;
-}
-
-
-public String getNom() {
-	return this.nom;
-}
-
-public String getPrenom() {
-	return this.prenom;
-}
-
-public String getLogin() {
-	return this.login;
-}
-
-public String getPassword() {
-	return this.password;
-}
-
-public void setNom(String nom) {
-	this.nom = nom;
-}
-
-public void setPrenom(String prenom) {
-	this.prenom = prenom;
-}
-
-public void setLogin(String login) {
-	this.login = login;
-}
-
-public void setPassword(String password) {
-	this.password = password;
-}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public String getPrenom() {
+		return this.prenom;
+	}
+	
+	public String getLogin() {
+		return this.login;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 
 
